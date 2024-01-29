34f36fac9480a1605f18c8d6c6ccc337e39ccf1e
friwid <50137120+friwid@users.noreply.github.com>
2024-01-29 21:54:28 +0100

    Fix main.css path

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import './main.css'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
