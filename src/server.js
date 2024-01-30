81183102fd7a627b647049382a3f2753152c15f8
friwid <50137120+friwid@users.noreply.github.com>
2024-01-30 13:13:11 +0100

    Add express-validator package
    And minor adjustments in server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+//import path from "path";
 
 const app = express();
 
+// Set directory to contain the templates ('views')
+//app.set("views", path.join(__dirname, "views"));
+
 //Register view engine
-app.set("vie engine", "ejs");
+// app.set("vie engine", "ejs");
 
 //Middleware and static files
-app.use(express.static("public"));
+//app.use(express.static("public"));
 app.use(morgan('dev'));
 
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
