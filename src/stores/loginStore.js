3ec7c7390d438373f6a3e807ab6833e2697b7393
friwid <50137120+friwid@users.noreply.github.com>
2024-02-05 13:38:31 +0100

    Add bcrypt Hashing to login and register stores.

--- src/stores/loginStore.js
+++ src/stores/loginStore.js
@@ -5,8 +5,17 @@ export const defineLoginStore = () => {
   const token = ref(null);
 
   const login = async ({ username, password }) => {
-    const response = await axios.post("/api/login", { username, password });
+
+    // use bcrypt
+    const passwordHash = await Bun.password.hash(password, {
+      algorithm: "bcrypt",
+      cost: 4, // number between 4-31
+    });
+
+    const response = await axios.post("/api/login", { username, passwordHash });
     token.value = response.data.token;
+
+    //TODO: Check password against stored hash in database: const isMatch = await Bun.password.verify(password, hash);
   };
 
   const logout = () => {
--- src/stores/registerStore.js
+++ src/stores/registerStore.js
@@ -5,7 +5,14 @@ export const defineRegisterStore = () => {
   const isRegistered = ref(false);
 
   const register = async ({ username, email, password }) => {
-    await axios.post("/api/register", { username, email, password });
+
+    // use bcrypt
+    const passwordHash = await Bun.password.hash(password, {
+      algorithm: "bcrypt",
+      cost: 4, // number between 4-31
+    });
+
+    await axios.post("/api/register", { username, email, passwordHash });
     isRegistered.value = true;
   };
 
